2d9c6b4219e6f39670d74f53833718541e361ad3
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-18 12:37:18 -0500

    chore: improvement on the assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+  mix.version();
+}
