87fe2a56d2be09b2234d1b10de9116a1889e5eed
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-18 12:58:04 -0500

    chore: add travis CI

--- config/database.php
+++ config/database.php
@@ -82,6 +82,17 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', 'localhost'),
+            'database' => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username' => env('DB_TEST_USERNAME', 'homestead'),
+            'password' => env('DB_TEST_PASSWORD', 'secret'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
     ],
 
     /*
