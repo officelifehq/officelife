d54899de923dab300787407f258dae9038856f3d
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-24 18:11:25 -0500

    chore: add services to create account and user

--- app/Models/Account/Account.php
+++ app/Models/Account/Account.php
@@ -9,6 +9,15 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Account extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'subdomain',
+    ];
+
+    /**
      * Get the user records associated with the account.
      *
      * @return HasMany
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
