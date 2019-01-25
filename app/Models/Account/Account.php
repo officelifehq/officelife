f789c67a607c3ff54413609eceac3cced1c99b50
Régis Freyd <djaiss@users.noreply.github.com>
2019-01-24 20:24:12 -0500

    chore: add automatic action logger (#3)

--- app/Models/Account/Account.php
+++ app/Models/Account/Account.php
@@ -4,10 +4,13 @@ namespace App\Models\Account;
 
 use App\Models\User\User;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
+    use LogsActivity;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -16,6 +19,13 @@ class Account extends Model
     protected $fillable = [
         'subdomain',
     ];
+
+    /**
+     * The attributes that are logged when changed.
+     *
+     * @var array
+     */
+    protected static $logAttributes = ['subdomain'];
 
     /**
      * Get the user records associated with the account.
