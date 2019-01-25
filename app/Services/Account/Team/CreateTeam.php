953aec6f1b6169d5d3e8cf3909cce940c3da9268
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-25 13:38:10 -0500

    WIP

---
+++ app/Services/Account/Team/CreateTeam.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Models\User\User;
+use App\Models\Account\Team;
+use App\Services\BaseService;
+use App\Models\Account\Account;
+use Illuminate\Support\Facades\Hash;
+
+class CreateTeam extends BaseService
+{
+    /**
+     * Get the validation rules that apply to the service.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'account_id' => 'required|integer|exists:accounts,id',
+            'name' => 'required|email|string',
+            'description' => 'required|string|max:255',
+        ];
+    }
+
+    /**
+     * Create a team.
+     *
+     * @param array $data
+     * @return Team
+     */
+    public function execute(array $data) : Team
+    {
+        $this->validate($data);
+
+        if (!$this->uniqueInAccount($data)) {
+            throw new EmailAlreadyUsedException;
+        }
+
+        return User::create([
+            'account_id' => $data['account_id'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'is_administrator' => $data['is_administrator'],
+        ]);
+    }
+}
