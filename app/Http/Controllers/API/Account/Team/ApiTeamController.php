a30db3148fad0a0e4649daca447f26014480da34
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-26 23:45:43 -0500

    fixes

--- app/Http/Controllers/API/Account/Team/ApiTeamController.php
+++ app/Http/Controllers/API/Account/Team/ApiTeamController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API\Account\Team;
 
 use App\Models\Account\Team;
 use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
 use App\Http\Controllers\Api\ApiController;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Resources\Account\Team\Team as TeamResource;
--- app/Http/Controllers/API/User/ApiUserController.php
+++ app/Http/Controllers/API/User/ApiUserController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\API\User;
 
 use App\Models\User\User;
 use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Resources\User\User as UserResource;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
