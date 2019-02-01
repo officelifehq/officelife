cae296b33163cf09cd78a5517ad432b40369f29f
Regis Freyd <djaiss@users.noreply.github.com>
2019-02-01 18:37:41 -0500

    chore: add account in route and remove dusk

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'account' => \App\Http\Middleware\CheckAccount::class,
     ];
 
     /**
---
+++ app/Http/Middleware/CheckAccount.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Account\Account;
+
+class CheckAccount
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // surprisingly, this gets the object, not the id
+        $account = $request->route()->parameter('account');
+
+        if (auth()->user()->account_id == $account->id) {
+            return $next($request);
+        }
+
+        abort(402);
+    }
+}
