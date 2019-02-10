7f0e6c11af67d686f1665c0959dd9f9395bbd5e2
Régis Freyd <djaiss@users.noreply.github.com>
2019-02-10 08:09:33 -0500

    chore: add purgecss (#32)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,7 +13,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css')
+  .purgeCss();
 
 if (mix.inProduction()) {
   mix.version();
