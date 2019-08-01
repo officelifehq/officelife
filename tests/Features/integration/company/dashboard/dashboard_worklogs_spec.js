169cfb6543dbda4220481750d3e37d405d75d3f3
Regis Freyd <djaiss@users.noreply.github.com>
2019-08-01 08:10:40 -0400

    final

--- tests/Features/integration/company/dashboard/dashboard_worklogs_spec.js
+++ tests/Features/integration/company/dashboard/dashboard_worklogs_spec.js
@@ -11,5 +11,19 @@ describe('Worklogs - employee', function () {
 
     cy.contains('You rock')
 
+    // assign a team to the employee
+    cy.createTeam('product')
+
+    cy.visit('/1/employees/1')
+
+    cy.get('[data-cy=open-team-modal-blank]').click()
+    cy.get('[data-cy=list-team-1]').click()
+
+    cy.visit('/1/dashboard')
+
+    cy.get('[data-cy=dashboard-team-tab]').click()
+
+    cy.get('body').should('contain', 'What your team has done this week')
+    cy.contains('I did two things today')
   })
 })
