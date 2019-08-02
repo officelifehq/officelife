20f37b19a25332861d2221e2d403d887af32a8c1
Regis Freyd <djaiss@users.noreply.github.com>
2019-08-02 07:50:46 -0400

    add table

---
+++ database/migrations/2019_08_02_114759_create_employee_statuses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_statuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->string('name');
+            $table->boolean('is_dummy')->default(false);
+            $table->timestamps();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+
+        Schema::table('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('employee_status_id')->after('position_id')->nullable();
+        });
+    }
+}
