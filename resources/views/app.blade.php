2b078f278d5bdded87a0358e66f857a6a1adee97
Regis Freyd <djaiss@users.noreply.github.com>
2019-10-08 21:41:30 -0400

    chore: remove meta header (#124)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}"/>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <script src="{{ mix('/js/app.js') }}" defer></script>
   <title>@yield('title', config('app.name'))</title>
