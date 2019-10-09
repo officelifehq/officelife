f4edc0d9baa5d784c5692bf6794e167abce311e0
Regis Freyd <djaiss@users.noreply.github.com>
2019-10-08 21:15:32 -0400

    fix: fix csrf mismatch

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,12 +16,6 @@ Vue.use(InertiaApp);
 window._ = require('lodash');
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
 
 // moment.js
 Vue.use(require('vue-moment'));
