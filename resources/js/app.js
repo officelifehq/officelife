22d7f098f2d355cf9fae0fb1cdff9735875933ce
Regis Freyd <djaiss@users.noreply.github.com>
2020-03-01 07:58:31 -0500

    more

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,10 +19,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // toaster
 window.events = new Vue();
-
 window.flash = function (message, level = 'success') {
   window.events.$emit('flash', { message, level });
 };
+
+import Snotify from 'vue-snotify';
+import 'vue-snotify/styles/simple.css';
+Vue.use(Snotify);
 
 // i18n
 import VueI18n from 'vue-i18n';
