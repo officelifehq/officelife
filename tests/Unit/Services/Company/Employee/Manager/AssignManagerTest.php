02bac645c22282f05625ec6d92ee84a9ffc0d4ad
Regis Freyd <djaiss@users.noreply.github.com>
2020-03-22 16:27:39 -0400

    wip

--- tests/Unit/Services/Company/Employee/Manager/AssignManagerTest.php
+++ tests/Unit/Services/Company/Employee/Manager/AssignManagerTest.php
@@ -9,19 +9,86 @@ use App\Models\Company\Employee;
 use App\Exceptions\SameIdsException;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Validation\ValidationException;
+use App\Exceptions\NotEnoughPermissionException;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Services\Company\Employee\Manager\AssignManager;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AssignManagerTest extends TestCase
 {
     use DatabaseTransactions;
 
     /** @test */
-    public function it_assigns_a_manager(): void
+    public function it_assigns_a_manager_as_administrator(): void
+    {
+        $dwight = $this->createAdministrator();
+        $this->executeService($dwight);
+    }
+
+    /** @test */
+    public function it_assigns_a_manager_as_hr(): void
+    {
+        $dwight = $this->createHR();
+        $this->executeService($dwight);
+    }
+
+    /** @test */
+    public function normal_user_cant_execute_the_service(): void
+    {
+        $this->expectException(NotEnoughPermissionException::class);
+
+        $dwight = $this->createEmployee();
+        $this->executeService($dwight);
+    }
+
+    /** @test */
+    public function it_fails_if_wrong_parameters_are_given(): void
+    {
+        $request = [
+            'first_name' => 'Dwight',
+        ];
+
+        $this->expectException(ValidationException::class);
+        (new AssignManager)->execute($request);
+    }
+
+    /** @test */
+    public function it_fails_if_employee_and_manager_are_the_same_person(): void
+    {
+        $dwight = factory(Employee::class)->create([]);
+
+        $request = [
+            'company_id' => $dwight->company_id,
+            'author_id' => $dwight->id,
+            'employee_id' => $dwight->id,
+            'manager_id' => $dwight->id,
+        ];
+
+        $this->expectException(SameIdsException::class);
+        (new AssignManager)->execute($request);
+    }
+
+    /** @test */
+    public function it_fails_if_the_manager_is_not_in_the_company(): void
+    {
+        $dwight = $this->createAdministrator();
+        $michael = $this->createAdministrator();
+
+        $request = [
+            'company_id' => $dwight->company_id,
+            'author_id' => $dwight->id,
+            'employee_id' => $dwight->id,
+            'manager_id' => $michael->id,
+        ];
+
+        $this->expectException(ModelNotFoundException::class);
+        (new AssignManager)->execute($request);
+    }
+
+    private function executeService(Employee $dwight): void
     {
         Queue::fake();
 
-        $dwight = factory(Employee::class)->create([]);
         $michael = factory(Employee::class)->create([
             'company_id' => $dwight->company_id,
         ]);
@@ -74,32 +141,5 @@ class AssignManagerTest extends TestCase
                     'direct_report_name' => $dwight->name,
                 ]);
         });
-    }
-
-    /** @test */
-    public function it_fails_if_wrong_parameters_are_given(): void
-    {
-        $request = [
-            'first_name' => 'Dwight',
-        ];
-
-        $this->expectException(ValidationException::class);
-        (new AssignManager)->execute($request);
-    }
-
-    /** @test */
-    public function it_fails_if_employee_and_manager_are_the_same_person(): void
-    {
-        $dwight = factory(Employee::class)->create([]);
-
-        $request = [
-            'company_id' => $dwight->company_id,
-            'author_id' => $dwight->id,
-            'employee_id' => $dwight->id,
-            'manager_id' => $dwight->id,
-        ];
-
-        $this->expectException(SameIdsException::class);
-        (new AssignManager)->execute($request);
     }
 }
