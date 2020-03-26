64e5c9f8f522663d6af991a7b351862b15d2a03e
Regis Freyd <djaiss@users.noreply.github.com>
2020-03-25 22:23:20 -0400

    Update UpdateWorkFromHomeInformationTest.php

--- tests/Unit/Services/Company/Employee/WorkFromHome/UpdateWorkFromHomeInformationTest.php
+++ tests/Unit/Services/Company/Employee/WorkFromHome/UpdateWorkFromHomeInformationTest.php
@@ -9,6 +9,7 @@ use App\Models\Company\Employee;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Validation\ValidationException;
+use App\Exceptions\NotEnoughPermissionException;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Services\Company\Employee\WorkFromHome\UpdateWorkFromHomeInformation;
 
@@ -17,58 +18,36 @@ class UpdateWorkFromHomeInformationTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function it_creates_a_new_work_from_home_entry(): void
+    public function it_creates_a_new_work_from_home_entry_as_administrator(): void
     {
-        Queue::fake();
+        $michael = $this->createAdministrator();
+        $dwight = $this->createAnotherEmployee($michael);
+        $this->executeService($michael, $dwight);
+    }
 
-        $michael = factory(Employee::class)->create([]);
+    /** @test */
+    public function it_creates_a_new_work_from_home_entry_as_hr(): void
+    {
+        $michael = $this->createHR();
+        $dwight = $this->createAnotherEmployee($michael);
+        $this->executeService($michael, $dwight);
+    }
 
-        $request = [
-            'company_id' => $michael->company_id,
-            'author_id' => $michael->id,
-            'employee_id' => $michael->id,
-            'date' => '2018-01-01',
-            'work_from_home' => true,
-        ];
+    /** @test */
+    public function user_can_execute_the_service_in_his_own_profile(): void
+    {
+        $michael = $this->createEmployee();
+        $this->executeService($michael, $michael);
+    }
 
-        $michael = (new UpdateWorkFromHomeInformation)->execute($request);
+    /** @test */
+    public function normal_user_cant_execute_the_service(): void
+    {
+        $this->expectException(NotEnoughPermissionException::class);
 
-        $this->assertInstanceOf(
-            Employee::class,
-            $michael
-        );
-
-        $this->assertDatabaseHas('employee_work_from_home', [
-            'employee_id' => $michael->id,
-            'date' => '2018-01-01 00:00:00',
-            'work_from_home' => true,
-        ]);
-
-        Queue::assertPushed(LogAccountAudit::class, function ($job) use ($michael) {
-            return $job->auditLog['action'] === 'employee_work_from_home_logged' &&
-                $job->auditLog['author_id'] === $michael->id &&
-                $job->auditLog['objects'] === json_encode([
-                    'employee_id' => $michael->id,
-                    'employee_name' => $michael->name,
-                    'date' => '2018-01-01',
-                ]);
-        });
-
-        Queue::assertPushed(LogEmployeeAudit::class, function ($job) use ($michael) {
-            return $job->auditLog['action'] === 'work_from_home_logged' &&
-                $job->auditLog['author_id'] === $michael->id &&
-                $job->auditLog['objects'] === json_encode([
-                    'date' => '2018-01-01',
-                ]);
-        });
-
-        // now we call the method again --- we should only have one entry in the
-        // database as the service doesn't create an additional entry
-        $michael = (new UpdateWorkFromHomeInformation)->execute($request);
-        $this->assertEquals(
-            1,
-            DB::table('employee_work_from_home')->where('employee_id', $michael->id)->count()
-        );
+        $michael = $this->createEmployee();
+        $dwight = $this->createAnotherEmployee($michael);
+        $this->executeService($michael, $dwight);
     }
 
     /** @test */
@@ -145,5 +124,57 @@ class UpdateWorkFromHomeInformationTest extends TestCase
 
         $this->expectException(ValidationException::class);
         (new UpdateWorkFromHomeInformation)->execute($request);
+    }
+
+    private function executeService(Employee $michael, Employee $dwight): void
+    {
+        Queue::fake();
+
+        $request = [
+            'company_id' => $michael->company_id,
+            'author_id' => $michael->id,
+            'employee_id' => $dwight->id,
+            'date' => '2018-01-01',
+            'work_from_home' => true,
+        ];
+
+        $dwight = (new UpdateWorkFromHomeInformation)->execute($request);
+
+        $this->assertInstanceOf(
+            Employee::class,
+            $dwight
+        );
+
+        $this->assertDatabaseHas('employee_work_from_home', [
+            'employee_id' => $dwight->id,
+            'date' => '2018-01-01 00:00:00',
+            'work_from_home' => true,
+        ]);
+
+        Queue::assertPushed(LogAccountAudit::class, function ($job) use ($michael, $dwight) {
+            return $job->auditLog['action'] === 'employee_work_from_home_logged' &&
+                $job->auditLog['author_id'] === $michael->id &&
+                $job->auditLog['objects'] === json_encode([
+                    'employee_id' => $dwight->id,
+                    'employee_name' => $dwight->name,
+                    'date' => '2018-01-01',
+                ]);
+        });
+
+        Queue::assertPushed(LogEmployeeAudit::class, function ($job) use ($michael) {
+            return $job->auditLog['action'] === 'work_from_home_logged' &&
+                $job->auditLog['author_id'] === $michael->id &&
+                $job->auditLog['objects'] === json_encode([
+                    'date' => '2018-01-01',
+                ]);
+        });
+
+        // now we call the method again --- we should only have one entry in the
+        // database as the service doesn't create an additional entry
+        $dwight = (new UpdateWorkFromHomeInformation)->execute($request);
+        $this->assertEquals(
+            1,
+            DB::table('employee_work_from_home')->where('employee_id', $dwight->id)->count()
+        );
     }
 }
