19a60f0ba5a42e5a9aaa4b382af07577bc5a362a
Regis Freyd <djaiss@users.noreply.github.com>
2020-06-01 00:16:33 -0400

    chore: dummy data async generation

--- app/Services/Company/Adminland/Company/ProvisionDefaultAccountData.php
+++ app/Services/Company/Adminland/Company/ProvisionDefaultAccountData.php
@@ -4,9 +4,6 @@ namespace App\Services\Company\Adminland\Company;
 
 use Carbon\Carbon;
 use App\Services\BaseService;
-use App\Models\Company\Employee;
-use App\Services\Company\Adminland\Position\CreatePosition;
-use App\Services\Company\Adminland\EmployeeStatus\CreateEmployeeStatus;
 use App\Services\Company\Adminland\CompanyPTOPolicy\CreateCompanyPTOPolicy;
 
 class ProvisionDefaultAccountData extends BaseService
@@ -32,36 +29,6 @@ class ProvisionDefaultAccountData extends BaseService
     public function execute(array $data): void
     {
         $this->validateRules($data);
-
-        // positions
-        $positions = [
-            trans('app.default_position_ceo'),
-            trans('app.default_position_sales_representative'),
-            trans('app.default_position_marketing_specialist'),
-            trans('app.default_position_front_end_developer'),
-        ];
-
-        foreach ($positions as $position) {
-            (new CreatePosition)->execute([
-                'company_id' => $data['company_id'],
-                'author_id' => $data['author_id'],
-                'title' => $position,
-            ]);
-        }
-
-        // employee status
-        $statuses = [
-            trans('app.default_employee_status_full_time'),
-            trans('app.default_employee_status_part_time'),
-        ];
-
-        foreach ($statuses as $status) {
-            (new CreateEmployeeStatus)->execute([
-                'company_id' => $data['company_id'],
-                'author_id' => $data['author_id'],
-                'name' => $status,
-            ]);
-        }
 
         // PTO policies
         $currentYear = Carbon::now();
