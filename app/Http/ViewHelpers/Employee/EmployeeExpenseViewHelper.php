c302403510c7fe12e0f325ab2d58afb0a1e83da9
Regis Freyd <djaiss@users.noreply.github.com>
2020-08-09 22:36:43 -0400

    wip

---
+++ app/Http/ViewHelpers/Employee/EmployeeExpenseViewHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\ViewHelpers\Employee;
+
+use App\Helpers\DateHelper;
+use App\Helpers\MoneyHelper;
+use App\Models\Company\Expense;
+use App\Models\Company\Employee;
+use Illuminate\Support\Collection;
+
+class EmployeeExpenseViewHelper
+{
+    /**
+     * Array containing the main statistics about the expenses of this employee.
+     *
+     * @param $expenses
+     * @return Collection
+     */
+    public static function stats(Employee $employee, $expenses): array
+    {
+        $acceptedExpenses = $expenses->filter(function ($expense) {
+            return $expense->status === Expense::ACCEPTED;
+        });
+
+        $numberOfAcceptedExpenses = $acceptedExpenses->count();
+
+        $numberOfRejectedExpenses = $expenses->filter(function ($expense) {
+            return $expense->status === Expense::REJECTED_BY_MANAGER ||
+                $expense->status === Expense::REJECTED_BY_ACCOUNTING
+            ;
+        })->count();
+
+        $numberOfInProgressExpenses = $expenses->count() - $numberOfRejectedExpenses - $numberOfAcceptedExpenses;
+
+        $reimbursedAmount = $acceptedExpenses->sum(function ($expense) {
+            $amount = $expense->converted_amount ? $expense->converted_amount : $expense->amount;
+            return $amount;
+        });
+
+        return [
+            'numberOfAcceptedExpenses' => $numberOfAcceptedExpenses,
+            'numberOfRejectedExpenses' => $numberOfRejectedExpenses,
+            'numberOfInProgressExpenses' => $numberOfInProgressExpenses,
+            'reimbursedAmount' => MoneyHelper::format($reimbursedAmount, $employee->company->currency),
+        ];
+    }
+
+    public static function list(Employee $employee, $expenses): Collection
+    {
+        $company = $employee->company;
+
+        $expensesCollection = collect([]);
+        foreach ($expenses as $expense) {
+            $expensesCollection->push([
+                'id' => $expense->id,
+                'title' => $expense->title,
+                'amount' => MoneyHelper::format($expense->amount, $expense->currency),
+                'status' => $expense->status,
+                'expensed_at' => DateHelper::formatDate($expense->expensed_at),
+                'converted_amount' => $expense->converted_amount ?
+                    MoneyHelper::format($expense->converted_amount, $expense->converted_to_currency) :
+                    null,
+                'url' => route('employee.expenses.show', [
+                    'company' => $company,
+                    'employee' => $employee,
+                    'expense' => $expense,
+                ]),
+            ]);
+        }
+
+        return $expensesCollection;
+    }
+}
---
+++ tests/Unit/ViewHelpers/Employee/EmployeeExpenseViewHelperTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Unit\ViewHelpers\Employee;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use App\Models\Company\Expense;
+use App\Models\Company\Employee;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Http\ViewHelpers\Employee\EmployeeExpenseViewHelper;
+
+class EmployeeExpenseViewHelperTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_gets_an_array_of_statistics_about_the_expenses_of_this_employee(): void
+    {
+        $michael = factory(Employee::class)->create([]);
+
+        // 2 accepted expenses
+        factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'amount' => 100,
+            'status' => Expense::ACCEPTED,
+        ]);
+        factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'amount' => 50,
+            'status' => Expense::ACCEPTED,
+        ]);
+
+        // 2 awaiting expenses
+        factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'status' => Expense::AWAITING_MANAGER_APPROVAL,
+        ]);
+        factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'status' => Expense::AWAITING_ACCOUTING_APPROVAL,
+        ]);
+
+        // 1 rejected expense
+        factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'status' => Expense::REJECTED_BY_ACCOUNTING,
+        ]);
+
+        $this->assertEquals(
+            [
+                'numberOfAcceptedExpenses' => 2,
+                'numberOfRejectedExpenses' => 1,
+                'numberOfInProgressExpenses' => 2,
+                'reimbursedAmount' => '$1.50',
+            ],
+            EmployeeExpenseViewHelper::stats($michael, $michael->expenses)
+        );
+    }
+
+    /** @test */
+    public function it_gets_a_collection_of_expenses(): void
+    {
+        Carbon::setTestNow(Carbon::create(2019, 1, 1, 7, 0, 0));
+
+        $michael = $this->createAdministrator();
+
+        $expense = factory(Expense::class)->create([
+            'employee_id' => $michael->id,
+            'created_at' => '2019-01-01 01:00:00',
+        ]);
+
+        $collection = EmployeeExpenseViewHelper::list($michael, $michael->expenses);
+
+        $this->assertEquals(
+            [
+                0 => [
+                    'id' => $expense->id,
+                    'title' => 'Restaurant',
+                    'amount' => '$1.00',
+                    'status' => 'created',
+                    'expensed_at' => 'Jan 01, 1999',
+                    'converted_amount' => null,
+                    'url' => env('APP_URL') . '/' . $michael->company_id . '/employees/' . $michael->id . '/expenses/' . $expense->id,
+                ],
+            ],
+            $collection->toArray()
+        );
+    }
+}
