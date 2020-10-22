f6a4f0dd2f421339e9b3879084060ce0fe244442
Regis Freyd <djaiss@users.noreply.github.com>
2020-10-22 17:00:46 -0400

    fix: fix urls for the help center

--- tests/Unit/HelpCenterTest.php
+++ tests/Unit/HelpCenterTest.php
@@ -15,8 +15,8 @@ class HelpCenterTest extends TestCase
         $links = config('officelife.help_links');
 
         foreach ($links as $key => $link) {
-            $url = config('officelife.help_center_url') . '/docs/' . $link;
-
+            $url = config('officelife.help_center_url').$link;
+            \Log::info($url);
             $ch = curl_init($url);
 
             curl_setopt($ch, CURLOPT_NOBODY, true);
