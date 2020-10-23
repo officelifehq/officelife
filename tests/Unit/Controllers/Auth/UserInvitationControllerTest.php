d105be65d1458af9acc3099ddf8d54ff1c76607c
Alexis Saettler <alexis@saettler.org>
2020-10-23 17:10:57 +0200

    fix

--- tests/Unit/Controllers/Auth/UserInvitationControllerTest.php
+++ tests/Unit/Controllers/Auth/UserInvitationControllerTest.php
@@ -14,47 +14,6 @@ class UserInvitationControllerTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function it_validates_invitation_link(): void
-    {
-        $employee = factory(Employee::class)->create([
-            'invitation_link' => 'link'
-        ]);
-
-        $response = $this->get('invite/employee/link');
-
-        dump($response);
-        $response->assertStatus(200);
-        $response->assertSee('Auth\/Invitation\/AcceptInvitationUnlogged');
-    }
-
-    /** @test */
-    public function it_unvalidates_wrong_invitation_link(): void
-    {
-        $employee = factory(Employee::class)->create([
-            'invitation_link' => 'link'
-        ]);
-
-        $response = $this->get('invite/employee/badlink');
-
-        $response->assertStatus(200);
-        $response->assertSee('Auth\/Invitation\/InvalidInvitationLink');
-    }
-
-    /** @test */
-    public function it_unvalidates_already_validated_invitation_link(): void
-    {
-        $employee = factory(Employee::class)->create([
-            'invitation_link' => 'link',
-            'invitation_used_at' => now()
-        ]);
-
-        $response = $this->get('invite/employee/link');
-
-        $response->assertStatus(200);
-        $response->assertSee('Auth\/Invitation\/InvitationLinkAlreadyAccepted');
-    }
-
-    /** @test */
     public function it_creates_a_user_invited(): void
     {
         Notification::fake();
@@ -76,9 +35,7 @@ class UserInvitationControllerTest extends TestCase
         $response->assertStatus(200);
 
         $employee->refresh();
-        $this->assertDatabaseHas('employees', [
-            'id' => $employee->id,
-        ]);
+        $this->assertNotEmpty($employee->invitation_used_at);
         $this->assertDatabaseHas('users', [
             'id' => $employee->user_id,
             'email' => 'jim.halpert@dundermifflin.com',
@@ -87,6 +44,39 @@ class UserInvitationControllerTest extends TestCase
         $user = User::find($employee->user_id);
 
         Notification::assertSentTo(
+            [$user],
+            VerifyEmail::class
+        );
+    }
+
+    /** @test */
+    public function it_link_an_existing_user_invited(): void
+    {
+        Notification::fake();
+
+        // be sure to have at least 2 users
+        $user = factory(User::class)->create([
+            'email' => 'jim.halpert@dundermifflin.com',
+        ]);
+
+        $employee = factory(Employee::class)->create([
+            'invitation_link' => 'link'
+        ]);
+
+        $params = [
+            'email' => 'jim.halpert@dundermifflin.com',
+            'password' => 'pam',
+        ];
+
+        $response = $this->post('invite/employee/link/join', $params);
+
+        $response->assertStatus(200);
+
+        $employee->refresh();
+        $this->assertNotEmpty($employee->invitation_used_at);
+        $this->assertEquals($user->id, $employee->user_id);
+
+        Notification::assertNotSentTo(
             [$user],
             VerifyEmail::class
         );
