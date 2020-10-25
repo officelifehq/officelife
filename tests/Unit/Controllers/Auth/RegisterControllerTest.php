6ff6f4a795be6e6d67df31c0f1807d7c25f7d610
Alexis Saettler <alexis@saettler.org>
2020-10-25 17:38:03 +0100

    add test

--- tests/Unit/Controllers/Auth/RegisterControllerTest.php
+++ tests/Unit/Controllers/Auth/RegisterControllerTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit\Controllers\Auth;
 
 use Tests\TestCase;
 use App\Models\User\User;
-use App\Models\Company\Employee;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
