6b4273a32014acacd8ea1f63da7eda62b237438f
Regis Freyd <djaiss@users.noreply.github.com>
2020-10-26 15:15:07 -0400

    wip

--- tests/Unit/ViewHelpers/Project/ProjectDecisionsViewHelperTest.php
+++ tests/Unit/ViewHelpers/Project/ProjectDecisionsViewHelperTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Unit\ViewHelpers\Project;
 
 use Tests\TestCase;
-use App\Helpers\DateHelper;
-use App\Models\Company\Project;
+use App\Models\Company\Employee;
+use App\Models\Company\ProjectDecision;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Http\ViewHelpers\Project\ProjectDecisionsViewHelper;
 
@@ -15,64 +15,41 @@ class ProjectDecisionsViewHelperTest extends TestCase
     /** @test */
     public function it_gets_a_collection_of_decisions(): void
     {
-        $michael = $this->createAdministrator();
-        $jim = $this->createAnotherEmployee($michael);
-        $project = factory(Project::class)->create([
-            'company_id' => $michael->company_id,
+        $projectDecision = factory(ProjectDecision::class)->create([]);
+        $michael = factory(Employee::class)->create([
+            'company_id' => $projectDecision->project->company_id,
         ]);
-        $project->employees()->attach([
-            $michael->id => [
-                'role' => 'developer',
-            ],
+        $jim = factory(Employee::class)->create([
+            'company_id' => $projectDecision->project->company_id,
         ]);
-        $project->employees()->attach([
-            $jim->id => [
-                'role' => 'ios dev',
-            ],
-        ]);
+        $projectDecision->deciders()->attach([$michael->id]);
+        $projectDecision->deciders()->attach([$jim->id]);
+        $collection = ProjectDecisionsViewHelper::decisions($projectDecision->project);
 
-        $array = ProjectDecisionsViewHelper::decisions($project);
         $this->assertEquals(
             [
                 0 => [
                     'id' => $michael->id,
                     'name' => $michael->name,
                     'avatar' => $michael->avatar,
-                    'role' => 'developer',
-                    'added_at' => DateHelper::formatDate($michael->created_at),
-                    'position' => [
-                        'id' => $michael->position->id,
-                        'title' => $michael->position->title,
-                    ],
                     'url' => env('APP_URL').'/'.$michael->company_id.'/employees/'.$michael->id,
                 ],
                 1 => [
                     'id' => $jim->id,
                     'name' => $jim->name,
                     'avatar' => $jim->avatar,
-                    'role' => 'ios dev',
-                    'added_at' => DateHelper::formatDate($jim->created_at),
-                    'position' => [
-                        'id' => $jim->position->id,
-                        'title' => $jim->position->title,
-                    ],
                     'url' => env('APP_URL').'/'.$jim->company_id.'/employees/'.$jim->id,
                 ],
             ],
-            $array['members']->toArray()
+            $collection->toArray()[0]['deciders']->toArray()
         );
         $this->assertEquals(
-            [
-                '0' => [
-                    'id' => $michael->id,
-                    'role' => 'developer',
-                ],
-                1 => [
-                    'id' => $jim->id,
-                    'role' => 'ios dev',
-                ],
-            ],
-            $array['roles']->toArray()
+            $projectDecision->id,
+            $collection->toArray()[0]['id']
+        );
+        $this->assertEquals(
+            $projectDecision->title,
+            $collection->toArray()[0]['title']
         );
     }
 }
