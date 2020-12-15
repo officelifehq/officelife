4865366de548fc22f6df1b5cddd5f0b7e5cba58d
Regis Freyd <djaiss@users.noreply.github.com>
2020-12-15 18:23:15 -0500

    final

--- tests/Unit/ViewHelpers/Adminland/AdminEmployeeStatusViewHelperTest.php
+++ tests/Unit/ViewHelpers/Adminland/AdminEmployeeStatusViewHelperTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\ViewHelpers\Adminland;
 
 use Tests\TestCase;
+use App\Models\Company\EmployeeStatus;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Http\ViewHelpers\Adminland\AdminEmployeeStatusViewHelper;
 
@@ -17,10 +18,12 @@ class AdminEmployeeStatusViewHelperTest extends TestCase
 
         $collection = AdminEmployeeStatusViewHelper::index($michael->company);
 
+        $status = EmployeeStatus::first();
+
         $this->assertEquals(
             [
                 0 => [
-                    'id' => 1,
+                    'id' => $status->id,
                     'name' => 'Permanent',
                     'type' => 'internal',
                     'type_translated' => 'internal',
