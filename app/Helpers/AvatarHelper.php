cb5f6d177701ff8ad2a77db31c45df9a14db09e0
Regis Freyd <djaiss@users.noreply.github.com>
2021-03-14 15:12:33 -0400

    wip

--- app/Helpers/AvatarHelper.php
+++ app/Helpers/AvatarHelper.php
@@ -10,14 +10,21 @@ class AvatarHelper
      * Get the avatar of the user, at the requested size if it exists.
      *
      * @var Employee
+     * @var int
      * @return string|null
      */
-    public static function getImage(Employee $employee): ?string
+    public static function getImage(Employee $employee, int $width = null): ?string
     {
         if (! $employee->avatar_file_id) {
             return 'https://ui-avatars.com/api/?name='.$employee->name;
         }
 
-        return $employee->avatar;
+        if ($width) {
+            $url = $employee->avatar->cdn_url.'/-/scale_crop/'.$width.'x'.$width.'/smart/';
+        } else {
+            $url = $employee->avatar->cdn_url;
+        }
+
+        return $url;
     }
 }
