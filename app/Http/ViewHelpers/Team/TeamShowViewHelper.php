0edb236175811671fd08aaad8cc1a5fe5046a2bf
Regis Freyd <djaiss@users.noreply.github.com>
2021-03-24 20:39:53 -0400

    wip

--- app/Http/ViewHelpers/Team/TeamShowViewHelper.php
+++ app/Http/ViewHelpers/Team/TeamShowViewHelper.php
@@ -5,6 +5,7 @@ namespace App\Http\ViewHelpers\Team;
 use App\Helpers\ImageHelper;
 use App\Models\Company\Team;
 use App\Helpers\StringHelper;
+use App\Models\Company\Company;
 use Illuminate\Support\Collection;
 
 class TeamShowViewHelper
@@ -114,5 +115,46 @@ class TeamShowViewHelper
         }
 
         return $shipsCollection;
+    }
+
+    /**
+     * Search all potential leads for the team.
+     *
+     * @param Company $company
+     * @param string $criteria
+     * @return Collection
+     */
+    public static function searchPotentialLead(Company $company, string $criteria): Collection
+    {
+        $potentialEmployees = $company->employees()
+            ->select('id', 'first_name', 'last_name')
+            ->notLocked()
+            ->where(function ($query) use ($criteria) {
+                $query->where('first_name', 'LIKE', '%'.$criteria.'%')
+                    ->orWhere('last_name', 'LIKE', '%'.$criteria.'%')
+                    ->orWhere('email', 'LIKE', '%'.$criteria.'%');
+            })
+            ->where('id', '!=', $employee->id)
+            ->orderBy('last_name', 'asc')
+            ->take(10)
+            ->get();
+
+        // remove the existing managers of this employee from the list
+        $existingManagersForTheEmployee = $employee->getListOfManagers();
+        $potentialEmployees = $potentialEmployees->diff($existingManagersForTheEmployee);
+
+        // remove the existing direct reports of this employee from the list
+        $existingDirectReportsForTheEmployee = $employee->getListOfDirectReports();
+        $potentialEmployees = $potentialEmployees->diff($existingDirectReportsForTheEmployee);
+
+        $employeesCollection = collect([]);
+        foreach ($potentialEmployees as $employee) {
+            $employeesCollection->push([
+                'id' => $employee->id,
+                'name' => $employee->name,
+            ]);
+        }
+
+        return $employeesCollection;
     }
 }
