5154b64002fe165976c9bd70a409528da80ba1b3
Regis Freyd <djaiss@users.noreply.github.com>
2021-04-02 20:57:44 -0400

    Update Group.php

--- app/Models/Company/Group.php
+++ app/Models/Company/Group.php
@@ -3,6 +3,7 @@
 namespace App\Models\Company;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -44,7 +45,7 @@ class Group extends Model
     /**
      * Get the meeting records associated with the group.
      *
-     * @return hasMany
+     * @return HasMany
      */
     public function meetings()
     {
