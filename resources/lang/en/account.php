0e6d6b2941908dee5a5b485ce4bc9d169d8b67a7
Regis Freyd <djaiss@users.noreply.github.com>
2021-04-14 23:49:35 -0400

    fix

--- resources/lang/en/account.php
+++ resources/lang/en/account.php
@@ -313,12 +313,9 @@ return [
     'log_meeting_decision_updated' => 'Updated a meeting decision about an agenda item in a meeting in the group called :group_name.',
     'log_file_added_to_project' => 'Added a file called :name in the project called :project_name.',
     'log_project_file_destroyed' => 'Deleted a file called :name in the project called :project_name.',
-<<<<<<< HEAD
     'log_add_guest_to_meeting' => 'Added :employee_name in a meeting of the project called :project_name.',
     'log_meeting_date_set' => 'Set the meeting date in a meeting of the project called :project_name.',
-=======
     'log_company_founded_date_updated' => 'Updated the founded date of the company to :founded_at.',
->>>>>>> main
 
     // employee logs
     'employee_log_employee_created' => 'Created this employee entry.',
