f224f93391a3daef5f319dde920abea065824f2e
OfficeLifeBot <84710085+OfficeLifeBot@users.noreply.github.com>
2021-05-24 18:24:52 +0200

    New translations welcome.php (French) [skip ci]

--- resources/lang/fr/welcome.php
+++ resources/lang/fr/welcome.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'hide_message_forever' => 'Hide this welcome tab forever',
-    'thanks' => 'Thanks for joining OfficeLife.',
-    'admin_line_1' => 'As you’ve added this company, you are effectively the administrator of this account. You will be able to add employees, edit teams and manage everything in it.',
-    'admin_line_2' => 'However, at this stage, your account is empty. OfficeLife is really useful when everything is properly set up. We know it’s a bit of a pain to do, so we’ve compiled a list of things you should do in this handy guide to make the most out of your account:',
-    'admin_guide_cta' => 'Read our easy, simple setup guide',
+    'hide_message_forever' => 'Masquer définitivement cet onglet de bienvenue',
+    'thanks' => 'Merci d’avoir rejoint OfficeLife.',
+    'admin_line_1' => 'Lorsque vous aurez ajouté cette entreprise, vous serez l’administrateur de ce compte. Vous serez en mesure d’ajouter des employés, de modifier des équipes et de gérer tout ce qui s’y trouve.',
+    'admin_line_2' => 'Cependant, à ce stade, votre compte est vide. OfficeLife est vraiment utile lorsque tout est correctement configuré. Nous savons que c’est un peu douloureux à faire, nous avons donc dressé une liste de ce que vous devriez faire dans ce guide pratique pour tirer le meilleur parti de votre compte :',
+    'admin_guide_cta' => 'Lisez notre guide de configuration simple et facile',
 ];
