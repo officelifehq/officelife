2e5d54f648346e6486eca930b4c8651d9f97ea2e
OfficeLifeBot <84710085+OfficeLifeBot@users.noreply.github.com>
2021-05-27 23:53:37 +0200

    Update source file passwords.php [skip ci]

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Password Reset Language Lines
@@ -12,11 +10,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
-
 ];
