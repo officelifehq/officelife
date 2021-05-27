19cfa0962b68d013349b09fe5f929e21daf37412
OfficeLifeBot <84710085+OfficeLifeBot@users.noreply.github.com>
2021-05-27 23:53:32 +0200

    Update source file dashboard.php [skip ci]

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'tab_me' => 'You',
     'tab_my_team' => 'Your team',
@@ -7,9 +6,7 @@ return [
     'tab_manager' => 'Manager area',
     'tab_hr' => 'HR area',
     'tab_timesheets' => 'Your timesheets',
-
     'blank_state' => 'You are not associated with a team at the moment.',
-
     'morale_title' => 'How do you feel?',
     'morale_success_message' => 'Thanks for telling us how you feel.',
     'morale_already_logged' => 'You have already indicated how you felt today. Come back tomorrow!',
@@ -17,12 +14,10 @@ return [
     'morale_emotion_normal' => 'It’s a normal day',
     'morale_emotion_good' => 'Best day ever',
     'morale_rules' => 'You can tell us once per day, and you can’t edit your submission. Also, your opinion will be completely anonymous.',
-
     'work_from_home_title' => 'Work from home',
     'work_from_home_cta' => 'I will or I have worked from home today.',
     'work_from_home_help' => 'You can change this status anytime. This is reset every night at midnight.',
     'work_from_home_success' => 'Thanks for letting us know.',
-
     'worklog_title' => 'What have you done today?',
     'worklog_placeholder' => 'Let your coworkers know what you’ve been up to today.',
     'worklog_already_logged' => 'You have already logged your work today.',
@@ -30,48 +25,37 @@ return [
     'worklog_cta' => 'Log your work',
     'worklog_entry_description' => 'Your manager and your team members (if you are assigned to a team) will be able to read this status. Also, you won’t be able to edit this status once it’s submitted.',
     'worklog_success_message' => 'The entry has been saved.',
-
     'team_worklog_title' => 'What your team has done this week',
     'team_worklog_today' => 'Today',
     'team_worklog_stat' => 'Team members who have logged their work:',
     'team_worklog_blank' => 'No one has posted a status',
-
     'team_birthdate_title' => 'Upcoming birthdays',
     'team_birthdate_blank' => 'No one has a birthday in the next 30 days.',
     'team_birthdate_date' => 'Birthday on {date}',
-
     'team_no_team_yet' => 'You are not associated with a team yet.',
     'team_not_allowed' => 'You are not allowed to view this team.',
     'team_dont_exist' => 'This team does not exist.',
     'team_viewing' => 'Viewing',
-
     'team_upcoming_hires' => 'Starts on :date',
     'team_upcoming_hires_with_position' => 'Starts on :date as :position',
-
     'team_work_from_home_title' => 'Who is working from home today',
     'team_work_from_home_blank' => 'No one is working from home today.',
-
     'team_hired_at_title' => 'New hires coming soon',
     'team_hired_at_date' => 'Starts on {date}',
     'team_hired_at_date_with_position' => 'Starts on {date} as {position}',
-
     'team_hired_at_anniversaries' => 'Upcoming work anniversaries',
     'team_hired_at_anniversary_detail' => 'Will celebrate {age} years in {company} on {date}',
-
     'question_title' => 'Know your colleagues',
     'question_cta' => 'Answer this question',
-    'question_number_of_answers' => '{number} colleague has already answered. | {number} colleague has already answered. | {number} colleagues have already answered.',
+    'question_number_of_answers' => '{number} colleague has already answered.|{number} colleagues have already answered.',
     'question_answer_help' => 'Everyone in the company will be able to read your answer.',
     'question_answer_submitted' => 'Thanks for answering the question.',
     'question_answer_updated' => 'The answer has been updated.',
     'question_answer_destroyed' => 'The answer has been deleted.',
     'question_answer_link' => 'View all answers of this question',
-
     'team_recent_ships' => 'The 3 latest accomplishments in this team',
     'team_recent_ship_list_blank' => 'The team hasn’t shipped anything recently.',
-
     'task_title' => 'Tasks',
-
     'expense_title' => 'Expenses',
     'expense_cta' => 'Log an expense',
     'expense_create_help' => 'No one in the company, except your manager and HR, will be able to see this expense.',
@@ -88,12 +72,10 @@ return [
     'expense_show_status_rejected_by_manager' => 'Rejected by manager',
     'expense_show_status_rejected_by_accounting' => 'Rejected by accounting',
     'expense_show_status_accepted' => 'Accepted',
-
     'accountant_awaiting_accounting_title' => 'Expenses that need accounting approval',
     'accountant_awaiting_manager_title' => 'Expenses that need manager approval',
     'accountant_employee' => 'Employee:',
     'accountant_manager' => 'Manager:',
-
     'one_on_ones_title' => 'One on One with your manager',
     'one_on_ones_cta' => 'Participate',
     'one_on_ones_talking_point_title' => 'Talking points',
@@ -115,7 +97,6 @@ return [
     'one_on_ones_next_entry' => 'Next entry',
     'one_on_ones_created_on' => 'Created on',
     'one_on_ones_manager_title' => 'One on One with your direct reports',
-
     'manager_expense_title' => 'Expenses that needs your approval',
     'manager_expense_blank_state' => 'You don’t have any expenses to validate.',
     'manager_expense_description' => 'As a manager, you need to approve the following expenses before they are reiumbursed.',
@@ -136,17 +117,14 @@ return [
     'manager_expense_detail_employee_position_blank' => 'No position',
     'manager_expense_detail_employee_status' => 'Status',
     'manager_expense_detail_employee_status_blank' => 'No status',
-
     'manager_rate_manager_current_survey' => 'Current active survey',
     'manager_rate_manager_response_rate' => '{rate}% response rate',
     'manager_rate_manager_respondants' => '{respondants} direct reports',
     'manager_rate_manager_blank_state' => 'You don’t have any previous surveys about your performance as a manager yet.',
-
     'manager_contract_renewal_dates_title' => 'External employees with a contract that ends soon',
     'manager_contract_renewal_dates_ends_on' => 'Ends on {date}',
     'manager_contract_renewal_dates_ended_on' => 'Ended {date}',
     'manager_contract_renewal_dates_ends_range' => 'In {count} days',
-
     'manager_timesheet_title' => 'Timesheet you need to approve',
     'manager_timesheet_blank_state' => 'No direct reports have submitted a timesheet for you to approve.',
     'manager_timesheet_approval_duration' => ':hours h :minutes',
@@ -155,7 +133,6 @@ return [
     'manager_timesheet_view_details' => 'View details',
     'manager_timesheet_summary_count' => 'You have {count} timesheets to approve.',
     'manager_timesheet_index_title' => 'Timesheets of your direct reports to approve',
-
     'accounting_expense_detail_cta' => 'Accept or reject this expense',
     'accounting_expense_detail_expense_section' => 'Expense details',
     'accounting_expense_detail_expense_type' => 'Expense type',
@@ -187,14 +164,12 @@ return [
     'accounting_accepted_rejected_expenses' => 'All expenses',
     'accounting_expense_detail_accoutant_section' => 'Accountant',
     'accounting_expense_detail_accoutant_name' => 'Accountant name',
-
     'expense_detail_status' => 'Accountant name',
     'expense_detail_status_manager_approval_title' => 'This expense is waiting for manager approval.',
     'expense_detail_status_accounting_approval_title' => 'This expense is waiting for the accounting department approval.',
     'expense_detail_status_rejected_by_manager_title' => 'This expense has been rejected by the manager for the following reason:',
     'expense_detail_status_rejected_by_accounting_title' => 'This expense has been rejected by the accounting department for the following reason:',
     'expense_detail_status_accepted_title' => 'This expense has been approved.',
-
     'rate_your_manager_title' => 'Rate your manager',
     'rate_your_manager_subtitle' => 'It’s the end of the month. It’s time to tell us how it goes with your manager, {name}.',
     'rate_your_manager_anonymous' => 'Your opinion will be completely anonymous.',
@@ -204,11 +179,9 @@ return [
     'rate_your_manager_thanks_add_comment_cta' => 'Add a comment',
     'rate_your_manager_thanks_add_comment_reveal_identity' => 'Reveal your identity to Roger so he can reply to you',
     'rate_your_manager_final_sucess_message' => 'Thanks for giving your opinion',
-
     'contract_renewal_date_title' => 'Contract renewal',
     'contract_renewal_date' => 'Your contract ends on {date}, which is in {count} days.',
     'contract_renewal_date_late' => 'Your contract ended on {date}, so you should take care of that.',
-
     'timesheet_title' => 'Your timesheets',
     'timesheet_rejected_timesheets' => 'The timesheets below have been rejected. Please review them.',
     'timesheet_approved_by' => 'Approved by',
@@ -229,12 +202,10 @@ return [
     'timesheet_create_project' => 'Create a project',
     'timesheet_create_choose_project' => 'Choose a project',
     'timesheet_create_choose_task' => 'Choose a task',
-
     'hr_timesheets_title' => 'Timesheets',
     'hr_timesheet_index_title' => 'All the timesheets that need approvals',
     'hr_timesheet_summary_count' => '{count} timesheets to approve for employees who don’t have a manager',
     'hr_timesheet_summary_blank' => 'There are no timesheets to approve for now.',
-
     'e_coffee_title' => 'E-coffee',
     'e_coffee_match_title' => 'This week, you have been matched with {name}.',
     'e_coffee_match_desc' => 'Get to know {firstname} by taking a coffee break for 15 min anytime this week!',
@@ -243,6 +214,5 @@ return [
     'e_coffee_match_cta' => 'Mark as happened',
     'e_coffee_match_happened_title' => 'Thanks for talking to {name} this week.',
     'e_coffee_match_happened_desc' => 'See you next week for a new match!',
-
     'project_title' => 'Active projects you are part of',
 ];
