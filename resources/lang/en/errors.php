ad97115b83a8be02741f6ec3403b8ca6cca09010
OfficeLifeBot <84710085+OfficeLifeBot@users.noreply.github.com>
2021-05-27 23:53:33 +0200

    Update source file errors.php [skip ci]

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     'error_401' => 'Unauthorized',
     'error_404' => 'Page Not Found',
     'error_403' => 'Forbidden',
@@ -9,7 +7,6 @@ return [
     'error_429' => 'Too Many Requests',
     'error_500' => 'Server Error',
     'error_503' => 'Service Unavailable',
-
     'message_401' => 'You are not authorized to see this page, you fool.',
     'message_404' => 'Sorry, the page you are looking for could not be found.',
     'message_403' => 'Sorry, you are forbidden from accessing this page.',
@@ -17,5 +14,4 @@ return [
     'message_429' => 'Too many requests, please calm down.',
     'message_500' => 'Whoops, something went wrong on our servers.',
     'message_503' => 'Sorry, we are doing some maintenance. Please check back soon.',
-
 ];
