cbb104961b3639475bb8dce307840d31bc27c41b
Weblate (bot) <hosted@weblate.org>
2021-06-03 18:45:55 +0200

    chore(i18n): translations update from Weblate (#951)

--- resources/lang/nb_NO/welcome.php
+++ resources/lang/nb_NO/welcome.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'hide_message_forever' => 'Hide this welcome tab forever',
-    'thanks' => 'Thanks for joining OfficeLife.',
-    'admin_line_1' => 'As you’ve added this company, you are effectively the administrator of this account. You will be able to add employees, edit teams and manage everything in it.',
-    'admin_line_2' => 'However, at this stage, your account is empty. OfficeLife is really useful when everything is properly set up. We know it’s a bit of a pain to do, so we’ve compiled a list of things you should do in this handy guide to make the most out of your account:',
-    'admin_guide_cta' => 'Read our easy, simple setup guide',
+    'hide_message_forever' => 'Skjul denne velkomstfanen for alltid',
+    'thanks' => 'Takk for at du bruker OfficeLife.',
+    'admin_line_1' => 'Siden du har lagt til denne bedriften, er du effektivt sett administrator for kontoen. Du vil kunne legge til ansatte, redigere lag, og håndtere alt i den.',
+    'admin_line_2' => 'På dette tidspunktet er kontoen din tom. OfficeLife er veldig nyttig når alt er satt opp, men det er litt plunder. Derfor har vi laget en liste over ting du bør gjøre i denne nyttige veiledningen:',
+    'admin_guide_cta' => 'Les vår enkle oppsettsveiledning',
 ];
