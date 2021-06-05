d10022a3476f8dcc6f3053ed4a258e30e350e342
Weblate (bot) <hosted@weblate.org>
2021-06-05 22:50:01 +0200

    chore(i18n): translations update from Weblate (#966)
    Co-authored-by: Alexis Saettler <alexis@saettler.org>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/officelife/account/fr/
    Translate-URL: https://hosted.weblate.org/projects/officelife/auth/fr/
    Translation: OfficeLife/account
    Translation: OfficeLife/auth
    
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -40,4 +40,5 @@ return [
     'invitation_unlogged_choice_login' => 'Connectez-vous à votre compte',
     'register_password_confirmation' => 'Confirmez votre mot de passe',
     'mismatch' => 'Le mot de passe fourni ne correspond pas à votre mot de passe actuel.',
+    'change_language' => 'Modifier la langue :',
 ];
