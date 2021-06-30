3bdf53fdaf5eed500a0f537d3c98f80dd6c7621a
Regis Freyd <djaiss@users.noreply.github.com>
2021-06-30 08:38:22 -0400

    Update database/factories/Company/CompanyInvoiceFactory.php
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- database/factories/Company/CompanyInvoiceFactory.php
+++ database/factories/Company/CompanyInvoiceFactory.php
@@ -25,7 +25,11 @@ class CompanyInvoiceFactory extends Factory
     {
         return [
             'company_id' => Company::factory(),
-            'usage_history_id' => CompanyDailyUsageHistory::factory(),
+            'usage_history_id' => function (array $attributes) {
+                return CompanyDailyUsageHistory::factory()->create([
+                    'company_id' => $attributes['company_id'],
+                ]);
+            }),
             'sent_to_customer' => false,
             'customer_has_paid' => false,
             'email_address_invoice_sent_to' => $this->faker->email,
