870f65912532fa54bbe42a146bf9962cc2d7ffb1
Regis Freyd <djaiss@users.noreply.github.com>
2021-07-03 21:37:15 -0400

    Update resources/js/Pages/Company/KB/Index.vue
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/js/Pages/Company/KB/Index.vue
+++ resources/js/Pages/Company/KB/Index.vue
@@ -50,7 +50,7 @@
                       <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
                     </svg>
 
-                    {{ $t('kb.index_number_pages', {count: wiki.count}) }}
+                    {{ $tc('kb.index_number_pages', wiki.count, {count: wiki.count}) }}
                   </li>
                   <li v-if="wiki.most_recent_page" class="di relative">
                     <svg class="date-icon relative mr2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
