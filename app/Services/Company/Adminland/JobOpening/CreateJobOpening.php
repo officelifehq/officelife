6234b25d1053882eb46f394ef8a766ad2323b3bd
Regis Freyd <djaiss@users.noreply.github.com>
2021-07-11 23:09:09 -0400

    create job opening service

---
+++ app/Services/Company/Adminland/JobOpening/CreateJobOpening.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Services\Company\Adminland\JobOpening;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use App\Jobs\LogAccountAudit;
+use App\Services\BaseService;
+use App\Models\Company\Employee;
+use App\Models\Company\Position;
+use App\Models\Company\JobOpening;
+
+class CreateJobOpening extends BaseService
+{
+    protected array $data;
+    protected Position $position;
+    protected JobOpening $jobOpening;
+    protected Employee $sponsor;
+
+    /**
+     * Get the validation rules that apply to the service.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'company_id' => 'required|integer|exists:companies,id',
+            'position_id' => 'required|integer|exists:positions,id',
+            'author_id' => 'required|integer|exists:employees,id',
+            'sponsored_by_employee_id' => 'required|integer|exists:employees,id',
+            'title' => 'required|string|max:255',
+            'reference_number' => 'nullable|string|max:255',
+            'description' => 'required|string|max:65535',
+        ];
+    }
+
+    /**
+     * Create a job opening.
+     *
+     * @param array $data
+     * @return JobOpening
+     */
+    public function execute(array $data): JobOpening
+    {
+        $this->data = $data;
+
+        $this->validate();
+        $this->create();
+        $this->log();
+
+        return $this->jobOpening;
+    }
+
+    private function validate(): void
+    {
+        $this->validateRules($this->data);
+
+        $this->author($this->data['author_id'])
+            ->inCompany($this->data['company_id'])
+            ->asAtLeastHR()
+            ->canExecuteService();
+
+        $this->position = Position::where('company_id', $this->data['company_id'])
+            ->findOrFail($this->data['position_id']);
+
+        $this->employee = Employee::where('company_id', $this->data['company_id'])
+            ->findOrFail($this->data['sponsored_by_employee_id']);
+    }
+
+    private function create(): void
+    {
+        $this->jobOpening = JobOpening::create([
+            'company_id' => $this->data['company_id'],
+            'position_id' => $this->data['position_id'],
+            'sponsored_by_employee_id' => $this->data['sponsored_by_employee_id'],
+            'title' => $this->data['title'],
+            'description' => $this->data['description'],
+            'reference_number' => $this->valueOrNull($this->data, 'reference_number'),
+            'slug' => Str::slug($this->data['title'], '-'),
+        ]);
+    }
+
+    private function log(): void
+    {
+        LogAccountAudit::dispatch([
+            'company_id' => $this->data['company_id'],
+            'action' => 'job_opening_created',
+            'author_id' => $this->author->id,
+            'author_name' => $this->author->name,
+            'audited_at' => Carbon::now(),
+            'objects' => json_encode([
+                'job_opening_id' => $this->jobOpening->id,
+                'job_opening_title' => $this->jobOpening->title,
+            ]),
+        ])->onQueue('low');
+    }
+}
---
+++ tests/Unit/Services/Company/Adminland/Position/CreateJobOpeningTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tests\Unit\Services\Company\Adminland\JobOpening;
+
+use Tests\TestCase;
+use App\Jobs\LogAccountAudit;
+use App\Models\Company\Employee;
+use App\Models\Company\Position;
+use App\Models\Company\JobOpening;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Validation\ValidationException;
+use App\Exceptions\NotEnoughPermissionException;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Services\Company\Adminland\Position\CreatePosition;
+use App\Services\Company\Adminland\JobOpening\CreateJobOpening;
+
+class CreateJobOpeningTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_creates_a_job_opening_as_administrator(): void
+    {
+        $michael = $this->createAdministrator();
+        $position = Position::factory()->create([
+            'company_id' => $michael->company_id,
+        ]);
+        $this->executeService($michael, $michael, $position);
+    }
+
+    /** @test */
+    public function it_creates_a_job_opening_as_hr(): void
+    {
+        $michael = $this->createHR();
+        $position = Position::factory()->create([
+            'company_id' => $michael->company_id,
+        ]);
+        $this->executeService($michael, $michael, $position);
+    }
+
+    /** @test */
+    public function normal_user_cant_execute_the_service(): void
+    {
+        $this->expectException(NotEnoughPermissionException::class);
+
+        $michael = $this->createEmployee();
+        $position = Position::factory()->create([
+            'company_id' => $michael->company_id,
+        ]);
+        $this->executeService($michael, $michael, $position);
+    }
+
+    /** @test */
+    public function it_fails_if_wrong_parameters_are_given(): void
+    {
+        $request = [
+            'title' => 'Assistant to the regional manager',
+        ];
+
+        $this->expectException(ValidationException::class);
+        (new CreatePosition)->execute($request);
+    }
+
+    /** @test */
+    public function it_fails_if_job_position_is_not_in_the_company(): void
+    {
+        $michael = $this->createAdministrator();
+        $position = Position::factory()->create([]);
+
+        $this->expectException(ModelNotFoundException::class);
+        $this->executeService($michael, $michael, $position);
+    }
+
+    private function executeService(Employee $author, Employee $sponsor, Position $position): void
+    {
+        Queue::fake();
+
+        $request = [
+            'company_id' => $author->company_id,
+            'author_id' => $author->id,
+            'position_id' => $position->id,
+            'sponsored_by_employee_id' => $sponsor->id,
+            'title' => 'Assistant to the regional manager',
+            'description' => 'Awesome job',
+            'reference_number' => '123',
+        ];
+
+        $jobOpening = (new CreateJobOpening)->execute($request);
+
+        $this->assertDatabaseHas('job_openings', [
+            'id' => $jobOpening->id,
+            'company_id' => $author->company_id,
+            'position_id' => $position->id,
+            'title' => 'Assistant to the regional manager',
+            'description' => 'Awesome job',
+            'slug' => 'assistant-to-the-regional-manager',
+            'reference_number' => '123',
+        ]);
+
+        $this->assertInstanceOf(
+            JobOpening::class,
+            $jobOpening
+        );
+
+        Queue::assertPushed(LogAccountAudit::class, function ($job) use ($author, $jobOpening) {
+            return $job->auditLog['action'] === 'job_opening_created' &&
+                $job->auditLog['author_id'] === $author->id &&
+                $job->auditLog['objects'] === json_encode([
+                    'job_opening_id' => $jobOpening->id,
+                    'job_opening_title' => $jobOpening->title,
+                ]);
+        });
+    }
+}
