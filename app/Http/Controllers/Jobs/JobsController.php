6d027be5f66cf08f064675b35255c8aefd965f5d
Regis Freyd <djaiss@users.noreply.github.com>
2021-08-02 14:42:00 -0400

    Update JobsController.php

--- app/Http/Controllers/Jobs/JobsController.php
+++ app/Http/Controllers/Jobs/JobsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Jobs;
 
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\ViewHelpers\Jobs\JobsIndexViewHelper;
 
