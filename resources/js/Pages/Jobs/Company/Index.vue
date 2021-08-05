1350e9817c850a161b6f861583d2a2c2cea8f514
Regis Freyd <djaiss@users.noreply.github.com>
2021-08-04 20:48:56 -0400

    wiip

--- resources/js/Pages/Jobs/Company/Index.vue
+++ resources/js/Pages/Jobs/Company/Index.vue
@@ -40,7 +40,7 @@ svg {
       <img v-if="data.company.logo" :src="data.company.logo" class="mr3" :alt="data.company.name" />
       <div>
         <h3>{{ data.company.name }}</h3>
-        <ul class="list pl0 ma0">
+        <ul v-if="data.company.location" class="list pl0 ma0">
           <li class="di mr2 relative">
             <svg xmlns="http://www.w3.org/2000/svg" class="mr1 relative" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
