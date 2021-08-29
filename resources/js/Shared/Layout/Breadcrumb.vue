df4bba5429ae3e8916105a88481fa160ca7abd85
Alexis Saettler <alexis@saettler.org>
2021-08-29 14:47:48 +0200

    refactor: create breadcrumb template

---
+++ resources/js/Shared/Layout/Breadcrumb.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mt4-l mt1 mw6 br3 bg-white box center breadcrumb relative z-0 f6 pb2">
+    <ul class="list ph0 tc-l tl">
+      <li class="di">
+        <inertia-link :href="crootUrl">{{ croot }}</inertia-link>
+      </li>
+      <li v-if="hasMore" class="di">
+        …
+      </li>
+      <li v-if="previous" class="di">
+        <inertia-link :href="previousUrl">{{ previous }}</inertia-link>
+      </li>
+      <li class="di">
+        <slot></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hasMore: {
+      type: Boolean,
+      default: false,
+    },
+    rootUrl: {
+      type: String,
+      default: null,
+    },
+    root: {
+      type: String,
+      default: null,
+    },
+    previousUrl: {
+      type: String,
+      default: null,
+    },
+    previous: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    crootUrl: function () {
+      return this.rootUrl ?? this.route('company.index', { company: this.$page.props.auth.company.id });
+    },
+
+    croot: function () {
+      return this.root ?? this.$t('app.breadcrumb_company');
+    }
+  }
+};
+</script>
