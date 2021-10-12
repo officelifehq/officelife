ac79554dbbebc1facf941c381ee46050d21cfc6a
Regis Freyd <djaiss@users.noreply.github.com>
2021-10-11 21:23:22 -0400

    feat: services to manage project issues

---
+++ database/migrations/2021_10_12_011717_create_issues_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('project_issues', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('reporter_id')->nullable();
+            $table->string('title');
+            $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('reporter_id')->references('id')->on('employees')->onDelete('set null');
+        });
+    }
+}
