c21953ae3f67ca4072306b0ecb74d162ffdc21d6
Regis Freyd <djaiss@users.noreply.github.com>
2021-10-23 18:17:10 -0400

    Update ProjectIssue.php

--- app/Models/Company/ProjectIssue.php
+++ app/Models/Company/ProjectIssue.php
@@ -4,6 +4,7 @@ namespace App\Models\Company;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
