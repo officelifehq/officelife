878e14f1ebe30bc5ca37c76b867db35ef3d5e7dd
Regis Freyd <djaiss@users.noreply.github.com>
2021-10-25 18:54:47 -0400

    Update ProjectBoard.php

--- app/Models/Company/ProjectBoard.php
+++ app/Models/Company/ProjectBoard.php
@@ -4,6 +4,7 @@ namespace App\Models\Company;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
