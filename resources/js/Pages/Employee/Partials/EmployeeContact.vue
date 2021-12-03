69ab54efe4a8193120d5d711d81f78b54823ca8b
Regis Freyd <djaiss@users.noreply.github.com>
2021-12-02 22:16:13 -0500

    chore: replace popup on edit pronoun on employee profile page (#1432)

--- resources/js/Pages/Employee/Partials/EmployeeContact.vue
+++ resources/js/Pages/Employee/Partials/EmployeeContact.vue
@@ -14,7 +14,7 @@
 <template>
   <div class="mb4 relative">
     <div class="db fw4 mb3 relative">
-      <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <svg class="icon mr1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
       </svg>
       <span class="f6 title">
