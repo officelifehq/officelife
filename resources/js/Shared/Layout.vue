19ba4dfe207a78825e3026876c5da1618fea8fab
Regis Freyd <djaiss@users.noreply.github.com>
2021-12-15 09:42:22 -0500

    chore: display congrats message on the footer

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -226,7 +226,7 @@ nav {
       </span> Welcome to OfficeLife beta! <a href="https://github.com/officelifehq/officelife/discussions/944" class="mr2">Contact us to send feedback</a>
       <span class="mr1">
         🎉
-      </span><a href="https://twitter.com/djaiss/status/1306414432340373504">What's new</a>
+      </span>Thanks for using our tool!
     </div>
   </div>
 </template>
